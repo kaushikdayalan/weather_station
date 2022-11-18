090f196109f6ef84cecf9b78973303a75034ccfa
kaushikdayalan <kaushikruth1@gmail.com>
2022-11-19 00:36:02 +0100

    production build backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const weatherStationRouter = require('./routes/weather');
 
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use('/weather', weatherStationRouter);
 
 app.get('/', (req, res) =>{
+    res.status(200);
     res.send('<h1>Node.js CURD API for the weather station</h1><h4>Message: Success</h4><p>Version: 1.0</p>')
 })
 
